58d62b5492a1f499ce81323b6f77f3a4e93b25c9
Paolo Rechia <paolorechia@gmail.com>
2023-04-01 21:37:33 +0200

    Use standard error output

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let args = grep_clone::Args::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     let result = grep_clone::run(args);
 
     if let Err(e) = result {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
