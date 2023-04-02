7b5f8bf86b84d6da290474218a4e25c0be4e4b5f
Paolo Rechia <paolorechia@gmail.com>
2023-04-02 03:34:53 +0200

    insensitive search to iterator

--- src/lib.rs
+++ src/lib.rs
@@ -43,14 +43,13 @@ impl Args {
 
 pub fn case_insensitive_search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut result: Vec<&str> = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-        result.push(line);
-        }
-    }
-    result
+    contents
+        .lines()
+        .filter(|line| {
+            line.to_lowercase().contains(&query)
+        })
+        .collect()
 }
 
 
